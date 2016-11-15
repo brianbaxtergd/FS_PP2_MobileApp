0037a6f2a9809d6ed9b9ca722a7bb6c42870aff7
Martin Herrera <MHerreraEraso@fullsail.edu>
2016-11-15 16:26:00 -0500

    Commit including ProjectSettings.asset

--- Assets/Scripts/Project.cs
+++ Assets/Scripts/Project.cs
@@ -1,62 +1,62 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿//using UnityEngine;
+//using System.Collections;
 
-public class Project : MonoBehaviour
-{
-    // Data.
-    string mName;
-    Color mColor;
-    ArrayList mTaskList;
-    ArrayList mArchivedTaskList;
+//public class Project : MonoBehaviour
+//{
+//    // Data.
+//    string mName;
+//    Color mColor;
+//    ArrayList mTaskList;
+//    ArrayList mArchivedTaskList;
 
-    // Constructors.
-    public Project()
-    {
-        mName = "Default Project";
-        Color mColor = Color.white;
-        mTaskList = new ArrayList();
-        mArchivedTaskList = new ArrayList();
-    }
-    public Project(string _name, Color _color)
-    {
-        mName = _name;
-        mColor = _color;
-    }
-    // Tasklist interface.
-    public int GetTaskCount()
-    {
-        return mTaskList.Count;
-    }
-    public void AddTask(string _taskName, int _priority)
-    {
-        Task t = new Task(_taskName, _priority);
-        mTaskList.Insert(0, t);
-    }
-    public void CompleteTask(int _index)
-    {
-        if (_index >= 0 && _index < mTaskList.Count)
-        {
-            mArchivedTaskList.Add(mTaskList[_index]);
-            mTaskList.RemoveAt(_index);
-        }
-    }
-    public void DeleteTask(int _index)
-    {
-        if (_index >= 0 && _index < mTaskList.Count)
-            mTaskList.RemoveAt(_index);
-    }
-    // Unity function.
-    void Start()
-    {
+//    // Constructors.
+//    public Project()
+//    {
+//        mName = "Default Project";
+//        Color mColor = Color.white;
+//        mTaskList = new ArrayList();
+//        mArchivedTaskList = new ArrayList();
+//    }
+//    public Project(string _name, Color _color)
+//    {
+//        mName = _name;
+//        mColor = _color;
+//    }
+//    // Tasklist interface.
+//    public int GetTaskCount()
+//    {
+//        return mTaskList.Count;
+//    }
+//    public void AddTask(string _taskName, int _priority)
+//    {
+//        Task t = new Task(_taskName, _priority);
+//        mTaskList.Insert(0, t);
+//    }
+//    public void CompleteTask(int _index)
+//    {
+//        if (_index >= 0 && _index < mTaskList.Count)
+//        {
+//            mArchivedTaskList.Add(mTaskList[_index]);
+//            mTaskList.RemoveAt(_index);
+//        }
+//    }
+//    public void DeleteTask(int _index)
+//    {
+//        if (_index >= 0 && _index < mTaskList.Count)
+//            mTaskList.RemoveAt(_index);
+//    }
+//    // Unity function.
+//    void Start()
+//    {
 
-    }
-    void Update()
-    {
+//    }
+//    void Update()
+//    {
 
-    }
-    // Rendering.
-    public void RenderTaskList()
-    {
+//    }
+//    // Rendering.
+//    public void RenderTaskList()
+//    {
 
-    }
-}
+//    }
+//}
