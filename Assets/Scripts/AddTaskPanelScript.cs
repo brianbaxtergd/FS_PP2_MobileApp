6a85a0da95a1e6c49a4b98a389c185cfb111471d
Brian Baxter <bbaxter1@fullsail.edu>
2017-01-12 19:03:09 -0500

    Bug fixes regarding Add New Task panel.

--- Assets/Scripts/AddTaskPanelScript.cs
+++ Assets/Scripts/AddTaskPanelScript.cs
@@ -6,19 +6,45 @@ using UnityEngine.UI;
 public class AddTaskPanelScript : MonoBehaviour
 {
     public InputField inputField;
+    public Text inputFieldText;
     public string text;
     public Button ok;
     public GameObject ac;
     DataController dc;
-    // Use this for initialization
+
     void Start()
     {
         dc = ac.GetComponent<DataController>();
     }
-    // Update is called once per frame
+    void OnEnable()
+    {
+        // Reset panel to default state.
+
+        // Disable the OK button.
+        ok.gameObject.SetActive(false);
+        // Clear the project name input field text.
+        inputField.Select();
+        inputField.text = "";
+    }
     void Update()
     {
+        // Update local string "text" with whatever user has entered.
         text = inputField.textComponent.text;
+
+        // Set OK-button's active-state contingent on user input.
+            // OK-button is active once user has provided values for all fields.
+        if (inputFieldText.text != "")
+        {
+            // Enable the OK button.
+            if (!ok.IsActive())
+                ok.gameObject.SetActive(true);
+        }
+        else
+        {
+            // Disable the OK button.
+            if (ok.IsActive())
+                ok.gameObject.SetActive(false);
+        }
     }
     public void OkPressed()
     {
